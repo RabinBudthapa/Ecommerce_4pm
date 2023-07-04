b84b903abd70bb11f36f23b82bf5f53f53964af3
RABIN BUDTHAPA <rabinbudthapa@RABINs-MacBook-Pro.local>
2023-07-04 16:35:31 +0545

    first commit

--- home/views.py
+++ home/views.py
@@ -12,4 +12,14 @@ class HomeView(Base):
         self.views['brands']=Brand.objects.all()
         self.views['sliders']=Slider.objects.all()
         self.views['reviews'] = Review.objects.all()
+        self.views['hots'] = Product.objects.filter(labels='hot')
+        self.views['news'] = Product.objects.filter(labels='new')
+        self.views['ads']=Product.objects.all()
         return render(request,'index.html',self.views)
+
+
+class CategoryView(Base):
+    def get(self,request,slug):
+        cat_id=Category.objects.get(slug=slug).id
+        self.views['cat_products']=Product.objects.filter(category_id=cat_id)
+        return render(request,'category.html',self.views)
